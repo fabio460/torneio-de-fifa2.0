1b44a8f56d0b4b370e08591380e7f04f2ce3d5e8
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-05-05 11:08:31 -0300

    instalando pacotes 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import TelaPrincipal from './telaPrincipal'
+import RotaPrivada from './rotaPrivada'
+import Login from './login'
+import Cadastro from './cadastro'
+import TelaDeElenco from './telaDeElenco'
+import TelaCompraDeJogadores from './telaCompraDeJogadores'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          olaaa mundo de novo
-      </header>
+    <div>
+        <BrowserRouter>
+          <Routes>
+              <Route path='/' element={<RotaPrivada><TelaPrincipal/></RotaPrivada>}></Route>
+              <Route path='/login' element={<Login/>}></Route>
+              <Route path='/elenco' element={<RotaPrivada><TelaDeElenco/></RotaPrivada>}></Route>
+              <Route path='/compraDeJogadores' element={<RotaPrivada><TelaCompraDeJogadores/></RotaPrivada>}></Route>
+              <Route path='/cadastro' element={<Cadastro/>}></Route>
+          </Routes>
+        </BrowserRouter>  
     </div>
-  );
+  )
 }
-
-export default App;
