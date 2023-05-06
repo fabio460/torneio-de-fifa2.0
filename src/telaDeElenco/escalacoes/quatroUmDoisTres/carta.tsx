a5612cbdb0b8064ae4afaa1928d13f6c5f730418
fabio dasilva de oliveira <dasilvadeoliveirafabio@gmail.com>
2023-05-06 19:01:36 -0300

    css da carta

--- src/telaDeElenco/escalacoes/quatroUmDoisTres/carta.tsx
+++ src/telaDeElenco/escalacoes/quatroUmDoisTres/carta.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { jogadoresType } from '../../../types'
+import { getPosicaoPrincipal, traduzirPosicao } from '../../../telaCompraDeJogadores/Jogadores/metodosUteis'
 export default function Carta({elem}:{elem:jogadoresType}) {
   const handleJogador = (e:any)=>{
-    console.log(e)
+    
   }
   return (
     <div  id={elem.id}>
       <div className='cartaContainer'>
-        <img src={elem.imagemDoJogador} className='carta'/>
+        <div className='carta'>
+           <div className='cartaBody'>
+             <div className='overall'>{elem.overall}</div>
+             <div className='posicao'>{getPosicaoPrincipal(elem.posicao)}</div>
+             <img className='imgPais' src={elem.imagemDaNacionalidade} />
+             <img  className='imgTime' src={elem.escudoDoTime} />
+           </div>
+           <img src={elem.imagemDoJogador} className='cartaImg'/>
+        </div>
         <div className='cartaNome'>{elem.nome}</div>
       </div>
     </div>
