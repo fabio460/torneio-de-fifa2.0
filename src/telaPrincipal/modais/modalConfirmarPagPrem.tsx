3aadb0ee28d0ed7bed1041faba1068fbd0bfc07d
fabio460 <dasilvadeoliveirafabio@gmail.com>
2023-09-05 10:50:06 -0300

    ajuste no layout

--- src/telaPrincipal/modais/modalConfirmarPagPrem.tsx
+++ src/telaPrincipal/modais/modalConfirmarPagPrem.tsx
@@ -78,7 +78,7 @@ export default function ModalConfirmarPagamentoPremiacao({usuario, icone}:{
     width:"100%",
     "@media (max-width:800px)":{
       marginRight:"0px",
-      marginBottom:"10px"
+      marginBottom:"20px"
     }
   }
   return (
