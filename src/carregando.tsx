4fe3a1cffbc0f54ff0af9fd26249ce12349274ef
fabio460 <dasilvadeoliveirafabio@gmail.com>
2023-09-05 14:41:06 -0300

    ajuste no layout

--- src/carregando.tsx
+++ src/carregando.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
+
 
 export default function Carregando() {
   return (
-      <div className='carregando' style={{ height:"90vh", display:"flex", alignItems:"center", justifyContent:"center"}}>
+      <div className='carregando' style={{ height:"50vh", display:"flex", alignItems:"center", justifyContent:"center"}}>
         <CircularProgress/>
       </div>
   )
