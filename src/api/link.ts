aa90631b3ef3a4438c736e00348195084c3a862c
fabio460 <dasilvadeoliveirafabio@gmail.com>
2023-10-17 17:40:55 -0300

    almento das premiações

--- src/api/link.ts
+++ src/api/link.ts
@@ -1,2 +1,2 @@
-// export const linkLocal = "https://gerencia-torneio-fifa-backend.vercel.app/"
- export const linkLocal = "http://localhost:4000/"
+ export const linkLocal = "https://gerencia-torneio-fifa-backend.vercel.app/"
+// export const linkLocal = "http://localhost:4000/"
